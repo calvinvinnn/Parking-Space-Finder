572c3f1505676876dba4fb28bad921415207cbf2
calvinvinnn <christofer.calvin99@gmail.com>
2020-02-08 11:27:54 -0800

    Create screen design
    xml files for : Welcome, Registration, Login, Main Menu, Book

--- app/src/main/java/com/example/parking_space_finder/WelcomeActivity.java
+++ app/src/main/java/com/example/parking_space_finder/WelcomeActivity.java
@@ -9,12 +9,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
